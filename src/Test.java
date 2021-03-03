cce5d5f603c0c92679aa03f20716346a8b7e5c78
LAPTOP-BS8VFGQL\xiaolan <1036850868@qq.com>
2021-03-03 15:36:30 +0800

    DataStruct Study

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,5 @@
 public class Test {
     public static void main(String... args) {
-        int a[][] = {{1,2},{2,3},{4,5},{5,6}};
-        for (int[] arr:a)
-            for (int i: arr)
-                System.out.print(i+"\t");
+
     }
 }
