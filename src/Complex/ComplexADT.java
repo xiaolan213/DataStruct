e5bf9e06a608e3870bddd93331e62709057c5d7e
LAPTOP-BS8VFGQL\xiaolan <1036850868@qq.com>
2021-03-05 21:27:46 +0800

    Calculate ComplexADT

---
+++ src/Complex/ComplexADT.java
@@ -0,0 +1,67 @@
+package Complex;
+
+public class ComplexADT {
+    private double realPart;
+    private double imagePart;
+
+    public ComplexADT() {
+    }
+
+    public ComplexADT(double realPart, double imagePart) {
+        this.realPart = realPart;
+        this.imagePart = imagePart;
+    }
+
+    public double getrealPart() {
+        return realPart;
+    }
+
+    public void setrealPart(double realPart) {
+        this.realPart = realPart;
+    }
+
+    public double getimagePart() {
+        return imagePart;
+    }
+
+    public void setimagePart(double imagePart) {
+        this.imagePart = imagePart;
+    }
+
+    public ComplexADT add(ComplexADT com){
+        judge(com);
+        double realPart = this.getrealPart() + com.getrealPart();
+        double imagePart = this.getimagePart() + com.getimagePart();
+        return new ComplexADT(realPart,imagePart);
+    }
+
+    public ComplexADT subtract(ComplexADT com){
+        judge(com);
+        double realPart = this.getrealPart() - com.getrealPart();
+        double imagePart = this.getimagePart() - com.getimagePart();
+        return new ComplexADT(realPart,imagePart);
+    }
+
+    public ComplexADT multiply(ComplexADT com){
+        judge(com);
+        //乘法公式(a+bi)(c+di)=(ac-bd)+(bc+ad)i
+        double realPart = (this.getrealPart()*com.getrealPart()-this.getimagePart()*com.getimagePart());
+        double imagePart = (this.getimagePart()*com.getrealPart()+this.getrealPart()*com.getimagePart());
+        return new ComplexADT(realPart,imagePart);
+    }
+
+    public ComplexADT judge(ComplexADT com){
+        if (com == null)
+            return new ComplexADT();
+        return com;
+    }
+
+    public String output(){
+        if (imagePart > 0)
+            return realPart + "+" + imagePart + "i";
+        else if (imagePart < 0)
+            return this.realPart + "+" + imagePart + "i";
+        else
+            return realPart + "";//虚数为0
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,28 @@
+import Complex.ComplexADT;
+
+import java.util.Scanner;
+
 public class Test {
     public static void main(String... args) {
+        double realPart = 0.0;
+        double imagePart = 0.0;
+        System.out.println("请输入第一个复数的实部和虚部：");
+        Scanner sc = new Scanner(System.in);
+        realPart = sc.nextDouble();
+        imagePart = sc.nextDouble();
+        ComplexADT complexNumber1 = new ComplexADT(realPart, imagePart);
 
+        System.out.println("请输入第二个复数的实部和虚部：");
+        realPart = sc.nextDouble();
+        imagePart = sc.nextDouble();
+        ComplexADT complexNumber2 = new ComplexADT(realPart, imagePart);
+
+        ComplexADT add = complexNumber1.add(complexNumber2);
+        ComplexADT sub = complexNumber1.subtract(complexNumber2);
+        ComplexADT mul = complexNumber1.multiply(complexNumber2);
+
+        System.out.println("两数相加：" + add.output());
+        System.out.println("两数相减：" + sub.output());
+        System.out.println("两数相乘：" + mul.output());
     }
 }
