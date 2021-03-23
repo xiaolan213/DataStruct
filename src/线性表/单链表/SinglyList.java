c7e47c6ee18f571cf1c176c98ee8fbca5314798c
LAPTOP-BS8VFGQL\xiaolan <1036850868@qq.com>
2021-03-23 19:19:27 +0800

    单链表类

--- src/线性表/单链表/SinglyList.java
+++ src/线性表/单链表/SinglyList.java
@@ -1,10 +1,9 @@
 package 线性表.单链表;
 
 //单链表类 继承object
-public class SinglyList<T> extends Object {
+public class SinglyList<T extends Comparable<? super T>> extends Object {
 
     public Node<T> head;//头指针,指向单链表的头节点
-
     //构造方法
     public SinglyList() {//构造空链表
         this.head = new Node<T>();//创建节点头 data和head均为Null
@@ -22,26 +21,82 @@ public class SinglyList<T> extends Object {
         }
     }
 
-    //反序单链表
-    public void reverse(T[] values){
-        for(int i=0; i <values.length; i++)
-            this.head.next = new Node<T>(values[i], this.head.next);
+    //通过名字查找
+    public Node<T> search(T key){
+        for(Node<T> p = this.head.next; p != null; p = p.next)
+            if (key.compareTo(p.data) == 0)
+                return p;
+        return null;
     }
+
+    //反序单链表
+    public void reverse(){
+        //判断链表的结点是否存在2个及以上
+        if (head == null || head.next == null)
+            return;
+        Node<T> temp = head.next;//临时结点
+        Node<T> next = null;
+        Node<T> pre = null;
+        while (temp.next != null) {
+            next = temp.next;
+            temp.next = pre;
+            pre = temp;
+            temp = next;
+        }
+        temp.next = pre;
+        this.head.next = temp;
+    }
+
+
+
+//    //逆置
+//    public void reverse() {
+//        if (isEmpty()){
+//            return;
+//        }
+//        reverse(head.next);
+//    }
+//    //反转指定的curr，并将反转后的结点返回
+//    public Node reverse(Node curr){
+//        if (curr.next == null){
+//            head.next = curr;
+//            return curr;
+//        }
+//        //递归的反转当前的结点的下一个结点。返回值就是链表反转后的当前结点的上一个结点
+//        Node pre = reverse(curr.next);
+//        //让返回的下一个结点b变为当前结点
+//        pre.next = curr;
+//        //把当前结点的下一个结点变为null
+//        curr.next = null;
+//        return curr;
+//    }
+
+
 
     //插入,插入x为第i个元素 x!=null 返回结点。i<0 则头插入,若i>长度尾巴插入
     public Node<T> insert(int i,T x){
         if (x == null)
             return null;
         Node<T> front = this.head;//front指向头结点
-        for (int j = 0; front.next != null && j < i; j++)//寻找i-1或最后一个结点(front指向)
-            front = front.next;
+        for (int j = 0; front.next != null && j < i; j++){
+            //寻找i-1或最后一个结点(front指向)
+                front = front.next;
+        }
         front.next = new Node<T>(x,front.next);//在front后插入值为x结点,包括头插入,中间/尾插入
         return front.next;
     }
 
-    //尾插入x
+    //排序插入
     public Node<T> insert(T x){
         //调用insert 用整数的最大值指定插入在最后
+        for (Node<T> i = this.head; i.next != null ; i = i.next){
+            if (i.next != null){
+                if (x.compareTo(i.next.data) <= 0){
+                    i.next = new Node<T>(x,i.next);
+                    return i.next;
+                }
+            }
+        }
         return insert(Integer.MAX_VALUE,x);
     }
 
@@ -58,15 +113,6 @@ public class SinglyList<T> extends Object {
         }
         return null;
     }
-
-    public Node<T> search(T key){
-        //Node<T> front = this.head;
-        for(Node<T> p = this.head.next; p != null; p = p.next)
-            if (key.equals(p.data))
-                return p;
-        return null;
-    }
-
 
     //判断单链表的下一个结点是否为空
     public boolean isEmpty(){
@@ -98,4 +144,5 @@ public class SinglyList<T> extends Object {
             str += p.data.toString()+(p.next != null ? "," : "");
         return str + ")";
     }
+
 }
